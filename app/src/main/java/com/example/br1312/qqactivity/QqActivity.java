7cd1eae740f72f066f37badee5f9749e26515030
BR1312 <xiao_bairui@163.com>
2017-11-14 21:37:39 +0800

    上传代码

--- app/src/main/java/com/example/br1312/qqactivity/QqActivity.java
+++ app/src/main/java/com/example/br1312/qqactivity/QqActivity.java
@@ -9,5 +9,6 @@ public class QqActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qq);
+        //测试上传
     }
 }
